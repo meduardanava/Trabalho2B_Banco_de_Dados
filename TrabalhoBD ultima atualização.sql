e9efcaef2e613f3462b8a2a7b843691522181557
Maria Eduarda Nava <114370938+meduardanava@users.noreply.github.com>
2023-06-20 22:54:10 -0300

    Update TrabalhoBD ultima atualização.sql

--- TrabalhoBD ultima atualização.sql
+++ TrabalhoBD ultima atualização.sql
@@ -8,40 +8,27 @@ CREATE TABLE alunos
 	telefone varchar(15) not null,
 	email varchar(50)not null
 );
-SELECT * FROM alunos
-select nome from alunos where id_aluno = 4
 
 INSERT INTO alunos(id_aluno, nome, sobrenome, data_nascimento, cpf, telefone, email)
 	 VALUES (1, 'João', 'Silva', '15/05/1990', '123.456.789-00', '(45) 1234-5678', 'joao.silva@edu.unipar.br');
-
 INSERT INTO alunos(id_aluno, nome, sobrenome, data_nascimento, cpf, telefone, email)
  	 VALUES (2, 'Maria', 'Santos', '30/09/1992', '987.654.321-00', '(45) 9876-5432', 'maria.santos@edu.unipar.br');
-
 INSERT INTO alunos(id_aluno, nome, sobrenome, data_nascimento, cpf, telefone, email)
 	 VALUES (3, 'Pedro', 'Ferreira', '10/03/1995', '456.789.123-00', '(45) 5555-5555', 'pedro.ferreira@edu.unipar.br');
-
 INSERT INTO alunos(id_aluno, nome, sobrenome, data_nascimento, cpf, telefone, email)
 	 VALUES (4, 'Ana', 'Oliveira', '25/07/1988', '789.123.456-00', '(45) 9999-9999', 'ana.oliveira@edu.unipar.br');
-
 INSERT INTO alunos(id_aluno, nome, sobrenome, data_nascimento, cpf, telefone, email)
 	 VALUES (5, 'Lucas', 'Rodrigues', '05/12/1989', '321.654.987-00', '(45) 4444-4444', 'lucas.rodrigues@edu.unipar.br');
-
 INSERT INTO alunos(id_aluno, nome, sobrenome, data_nascimento, cpf, telefone, email)
 	 VALUES (6, 'Juliana', 'Lima', '20/08/1991', '654.321.987-00', '(45) 7777-7777', 'juliana.lima@edu.unipar.br');
-
 INSERT INTO alunos(id_aluno, nome, sobrenome, data_nascimento, cpf, telefone, email)
 	 VALUES (7, 'Rafael', 'Costa', '12/02/1993', '987.123.456-00', '(45) 2222-2222', 'rafael.costa@edu.unipar.br');
-
 INSERT INTO alunos(id_aluno, nome, sobrenome, data_nascimento, cpf, telefone, email)
 	 VALUES (8, 'Carolina', 'Pereira', '28/06/1987', '456.789.123-00', '(45) 3333-3333', 'carolina.pereira@edu.unipar.br');
-
 INSERT INTO alunos(id_aluno, nome, sobrenome, data_nascimento, cpf, telefone, email)
 	 VALUES (9, 'Marcelo', 'Gomes', '08/11/1990', '789.123.456-00', '(45) 6666-6666', 'marcelo.gomes@edu.unipar.br');
-
 INSERT INTO alunos(id_aluno, nome, sobrenome, data_nascimento, cpf, telefone, email)
 	 VALUES (10, 'Fernanda', 'Martins', '18/04/1985', '321.654.987-00', '(45) 8888-8888', 'fernanda.martins@edu.unipar.br');
-
-	 
 
 CREATE TABLE professores
 (
@@ -53,8 +40,6 @@ CREATE TABLE professores
 	telefone varchar(15) not null,
 	email varchar(50) not null
 );
-
-SELECT * FROM professores
 
 INSERT INTO professores (id_professor, nome, sobrenome, data_nascimento, cpf, telefone, email)
 	 VALUES (1, 'Luciana', 'Ferreira', '20/11/1982', '456.789.123-00', '(45) 5555-5555', 'luciana.ferreira@prof.unipar.br');
@@ -77,8 +62,6 @@ CREATE TABLE materias
 	references professores(id_professor)
 );
 
-SELECT * FROM materias
-
 INSERT INTO materias(id_materia, nome_materia, idProfessor)
      VALUES (1, 'Estrutura e Classificação de Dados', 3);
 INSERT INTO materias(id_materia, nome_materia, idProfessor)
@@ -88,7 +71,6 @@ INSERT INTO materias(id_materia, nome_materia, idProfessor)
 INSERT INTO materias(id_materia, nome_materia, idProfessor)
      VALUES (4, 'Gestão de Projetos e Equipes', 2);
 
-
 CREATE TABLE periodos (
 	id_periodo int PRIMARY KEY NOT NULL,
 	periodo varchar(20) not null,
@@ -97,13 +79,10 @@ CREATE TABLE periodos (
 	REFERENCES materias (id_materia)
 );
 
-SELECT * FROM periodos
-
 INSERT INTO periodos(id_periodo, periodo, idMateria)
      VALUES (1, '3º Semestre', 3);
 INSERT INTO periodos(id_periodo, periodo, idMateria)
      VALUES (2, '3º Semestre', 2);
-
 
 CREATE TABLE notas
 (
@@ -119,7 +98,7 @@ CREATE TABLE notas
 	CONSTRAINT fk_periodos FOREIGN KEY (idPeriodo) 
 	references periodos(id_periodo)
 ); 
-delete from notas
+
 INSERT INTO notas(id_nota, idAluno, nota, idMateria, idPeriodo)
      VALUES (1, 3, 7.6, 2, 1);
 INSERT INTO notas(id_nota, idAluno, nota, idMateria, idPeriodo)
@@ -141,9 +120,6 @@ INSERT INTO notas(id_nota, idAluno, nota, idMateria, idPeriodo)
 INSERT INTO notas(id_nota, idAluno, nota, idMateria, idPeriodo)
      VALUES (10, 7, 3.3, 1, 1);
 
-select * from notas
-	 
-
 CREATE TABLE recuperacao (
     recuperacao_id INT GENERATED ALWAYS AS IDENTITY,
     idAluno int,
@@ -156,7 +132,6 @@ CREATE TABLE recuperacao (
 	CONSTRAINT fk_periodos FOREIGN KEY (idPeriodo)
 	REFERENCES periodos (id_periodo)
 );
-drop table recuperacao
 
 CREATE OR REPLACE FUNCTION checkNota()
 RETURNS TRIGGER AS $$
@@ -174,10 +149,12 @@ AFTER INSERT ON notas
 FOR EACH ROW
 EXECUTE FUNCTION checkNota();
 
-SELECT * FROM recuperacao;
 SELECT * FROM alunos;
+SELECT * FROM professores
+SELECT * FROM materias;
 SELECT * FROM periodos;
-
+SELECT * FROM notas;
+SELECT * FROM recuperacao;
 
 SELECT DISTINCT a.nome, a.sobrenome, n.nota
 FROM alunos a
